d271c3b2a6e8246a9c3b7c2e3a5c8367c79912c4
Zero <Zero@example.ru>
2022-09-18 21:11:02 +0600

    Решена Задача 56

--- HomeworkС#8/Program.cs
+++ HomeworkС#8/Program.cs
@@ -12,6 +12,7 @@
 // 9 5 3 2
 // 8 4 4 2
 
+/*
 int[,] CreateRandom2dArray(int rows, int cols, int min, int max)
 {
     int[,] array = new int[rows, cols];
@@ -68,4 +69,70 @@ Show2dArray(array);
 Console.WriteLine();
 int[,] array2 = YporadPoYbivanieStroki(array);
 Show2dArray(array2);
+*/
+
+// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и 
+// выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateRandom2dArray(int min, int max)
+{
+    int[,] array = new int[3, 4];
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 4; j++)
+            array[i, j] = new Random().Next(min, max + 1);
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void StrokaSNaimenSymmaElement(int[,] arr)
+{
+    int minsum = Int32.MaxValue;
+    int indexMass = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sum = sum + arr[i, j];
+        }
+        if (sum < minsum)
+        {
+            minsum = sum;
+            indexMass++;
+        }
+    }
+    Console.WriteLine("Cтрока с наименьшей суммой элементов в прямоугольном " +
+                       "двумерном массиве находится под номером: " +
+                     +(indexMass) + ", с суммой элементов которой равно: " + (minsum));
+
+}
+
+
+Console.Write("Введите минимум: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимум: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = CreateRandom2dArray(min, max);
+Show2dArray(array);
+Console.WriteLine();
+StrokaSNaimenSymmaElement(array);
 
