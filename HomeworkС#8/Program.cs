f18e9c00a30c7c4df9204f38450ef76638ba428d
Zero <Zero@example.ru>
2022-09-18 21:49:07 +0600

    Решена Задача 62

--- HomeworkС#8/Program.cs
+++ HomeworkС#8/Program.cs
@@ -80,7 +80,7 @@ Show2dArray(array2);
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и 
 // выдаёт номер строки с наименьшей суммой элементов: 1 строка
-
+/*
 int[,] CreateRandom2dArray(int min, int max)
 {
     int[,] array = new int[3, 4];
@@ -135,4 +135,54 @@ int[,] array = CreateRandom2dArray(min, max);
 Show2dArray(array);
 Console.WriteLine();
 StrokaSNaimenSymmaElement(array);
+*/
+
+// Задача 62.Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int[,] Array4x4Spiralno()
+{
+    int n = 4;
+    int[,] array4x4 = new int[n, n];
+
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+
+    while (temp <= array4x4.GetLength(0) * array4x4.GetLength(1))
+    {
+        array4x4[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < array4x4.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= array4x4.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > array4x4.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+
+    return array4x4;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Show2dArray(Array4x4Spiralno());
+Console.WriteLine();
+
 
